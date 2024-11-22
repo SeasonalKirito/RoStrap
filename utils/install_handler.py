3d7371e53ef91f9c9567f21dad0c1c46d124393f
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-11-22 10:30:57 -0700

    Update install_handler.py

--- utils/install_handler.py
+++ utils/install_handler.py
@@ -5,6 +5,33 @@ import requests
 from zipfile import ZipFile
 
 class Installer:
+    PACKAGE_MAP = {
+        "RobloxApp.zip": "",
+        "redist.zip": "",
+        "shaders.zip": "shaders/",
+        "ssl.zip": "ssl/",
+        "WebView2.zip": "",
+        "WebView2RuntimeInstaller.zip": "WebView2RuntimeInstaller/",
+        "content-avatar.zip": "content/avatar/",
+        "content-configs.zip": "content/configs/",
+        "content-fonts.zip": "content/fonts/",
+        "content-sky.zip": "content/sky/",
+        "content-sounds.zip": "content/sounds/",
+        "content-textures2.zip": "content/textures/",
+        "content-models.zip": "content/models/",
+        "content-platform-fonts.zip": "PlatformContent/pc/fonts/",
+        "content-platform-dictionaries.zip": "PlatformContent/pc/shared_compression_dictionaries/",
+        "content-terrain.zip": "PlatformContent/pc/terrain/",
+        "content-textures3.zip": "PlatformContent/pc/textures/",
+        "extracontent-luapackages.zip": "ExtraContent/LuaPackages/",
+        "extracontent-translations.zip": "ExtraContent/translations/",
+        "extracontent-models.zip": "ExtraContent/models/",
+        "extracontent-textures.zip": "ExtraContent/textures/",
+        "extracontent-places.zip": "ExtraContent/places/",
+        "RobloxPlayerLauncher.exe": "",
+    }
+
+    @staticmethod
     def _fetch_version_package_manifest(version):
         url = f"https://setup.rbxcdn.com/{version}-rbxPkgManifest.txt"
         response = requests.get(url)
@@ -14,12 +41,17 @@ class Installer:
             print(f"Failed to fetch version package manifest from {url}. Status code: {response.status_code}")
             return None
 
-    _versionPackageManifest = _fetch_version_package_manifest(ENUMS.get_latest_version())
+    def _get_packages(version):
+        package_manifest = Installer._fetch_version_package_manifest(version)
+        if package_manifest:
+            return package_manifest
+        else:
+            return None
 
     @staticmethod
     def install(version, clean):
         current_roblox = ENUMS.PATHS["ROBLOX_PATH"]
-        print(current_roblox)
+        print(f"Current Roblox path: {current_roblox}")
 
         if clean:
             for folder_name in os.listdir(current_roblox):
@@ -43,10 +75,12 @@ class Installer:
                 if response.status_code == 200:
                     with open(zip_path, 'wb') as file:
                         file.write(response.content)
+                    print(f"Downloaded {zip_file} successfully.")
 
                     # Unzip the file
                     with ZipFile(zip_path, 'r') as zip_ref:
                         zip_ref.extractall(os.path.join(version_path, extract_path))
+                    print(f"Extracted {zip_file} successfully.")
 
                     # Delete the zip file
                     os.remove(zip_path)
@@ -57,4 +91,5 @@ class Installer:
         else:
             print(f"Roblox version {version} already installed.")
 
+print(Installer._get_packages(ENUMS.get_latest_version()))
 Installer.install(ENUMS.get_latest_version(), True)
